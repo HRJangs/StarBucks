adae7257f4b0b65cf8321db1d8136ae409d26d35
ohyeon <oy9339@gmail.com>
2017-04-25 15:04:46 +0900

    dto emp추가

---
+++ src/dto/Emp.java
@@ -0,0 +1,56 @@
+package dto;
+
+public class Emp {
+	private int emp_id;
+	private String emp_login_id;
+	private String emp_login_pw;
+	private String emp_name;
+	private String emp_phone;
+	private String emp_job;
+	private int emp_sal;
+	
+	public int getEmp_id() {
+		return emp_id;
+	}
+	public void setEmp_id(int emp_id) {
+		this.emp_id = emp_id;
+	}
+	public String getEmp_login_id() {
+		return emp_login_id;
+	}
+	public void setEmp_login_id(String emp_login_id) {
+		this.emp_login_id = emp_login_id;
+	}
+	public String getEmp_login_pw() {
+		return emp_login_pw;
+	}
+	public void setEmp_login_pw(String emp_login_pw) {
+		this.emp_login_pw = emp_login_pw;
+	}
+	public String getEmp_name() {
+		return emp_name;
+	}
+	public void setEmp_name(String emp_name) {
+		this.emp_name = emp_name;
+	}
+	public String getEmp_phone() {
+		return emp_phone;
+	}
+	public void setEmp_phone(String emp_phone) {
+		this.emp_phone = emp_phone;
+	}
+	public String getEmp_job() {
+		return emp_job;
+	}
+	public void setEmp_job(String emp_job) {
+		this.emp_job = emp_job;
+	}
+	public int getEmp_sal() {
+		return emp_sal;
+	}
+	public void setEmp_sal(int emp_sal) {
+		this.emp_sal = emp_sal;
+	}
+	
+	
+}
